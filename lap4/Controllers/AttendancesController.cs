4337554542c25efa907544b75c85e1451f9b4367
Tnxkien <kienkienkien111@gmail.com>
2021-07-21 20:55:16 +0700

    4.3

--- lap4/Controllers/AttendancesController.cs
+++ lap4/Controllers/AttendancesController.cs
@@ -11,20 +11,31 @@ namespace lap4.Controllers
 {
     public class AttendancesController : ApiController
     {
-         public IHttpActionResult Attend(Course attendanceDto)
+        [HttpPost]
+        public IHttpActionResult Attend(Course attendanceDto)
         {
             var userID = User.Identity.GetUserId();
             BigSchoolContext context = new BigSchoolContext();
-            if (context.Attendances.Any(p=>p.Attendee == userID && p.CourseId == attendanceDto.Id))
+            if (context.Attendances.Any(p => p.Attendee == userID && p.CourseId ==
+            attendanceDto.Id))
             {
-                return BadRequest("The attendance already exist!");
+                // return BadRequest("The attendance already exists!");
+
+                // xóa thông tin khóa học đã đăng ký tham gia trong bảng Attendances
+                context.Attendances.Remove(context.Attendances.SingleOrDefault(p =>
+                p.Attendee == userID && p.CourseId == attendanceDto.Id));
+                context.SaveChanges();
+                return Ok("cancel");
             }
             var attendance = new Attendance()
-            { CourseId = attendanceDto.Id, Attendee = User.Identity.GetUserId() };
+            {
+                CourseId = attendanceDto.Id,
+                Attendee =
+            User.Identity.GetUserId()
+            };
             context.Attendances.Add(attendance);
             context.SaveChanges();
             return Ok();
-
         }
     }
 }
--- lap4/Models/Course.cs
+++ lap4/Models/Course.cs
@@ -15,27 +15,29 @@ namespace lap4.Models
             Attendances = new HashSet<Attendance>();
         }
 
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
+      
 
-        [Required]
         [StringLength(128)]
         public string LecturerId { get; set; }
 
-        [Required]
         [StringLength(255)]
         public string Place { get; set; }
 
         public DateTime DateTime { get; set; }
 
-        public int CategoryId { get; set; }
+        public int? CategoryId { get; set; }
+        public string Name;
+        public string LecturerName;
+        public bool isLogin = false;
+        public bool isShowGoing = false;
+        public bool isShowFollow = false;
+        public List<Category> ListCategory = new List<Category>();
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Attendance> Attendances { get; set; }
-        public string Name;
-        public string LecturerName;
-
-        public List<Category> ListCategory = new List<Category>();
         public virtual Category Category { get; set; }
+        
+   
     }
 }
